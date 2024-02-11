389e8b974a4a0e71b24220dce00adcedb0cef317
mynspluto <mynspluto@gmai.com>
2024-02-11 17:19:35 +0900

    api 테스트로 전환

---
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.example.productorderservice.product;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ProductRepository {
+
+    private Map<Long, Product> persistence = new HashMap();
+    private Long sequence = 0L;
+
+    public void save(final Product product) {
+        product.assiginId(++sequence);
+        persistence.put(product.getId(), product);
+    }
+}
