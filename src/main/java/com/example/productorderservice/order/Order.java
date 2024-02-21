11e107c5ca485bf884ff44a3d7a1c1a4cae084c3
mynspluto <mynspluto@gmai.com>
2024-02-21 18:09:57 +0900

    주문 Repository JPA 적용

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,13 +1,25 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private final Product product;
-    private final int quantity;
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         Assert.notNull(product, "상품은 필수입니다.");
